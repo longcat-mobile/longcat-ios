dd72fbe841f28e20f61fb451c5eb0512cd12d42f
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-02-15 03:35:18 +0300

    Update tools for Qt 5.12.1.

--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PATH=$PATH:~/Qt/5.9.1/ios/bin
+PATH=$PATH:~/Qt/5.12.1/ios/bin
 
 lupdate ../longcat.pro -ts ../translations/longcat_ru.src.ts
 lupdate ../qml         -ts ../translations/longcat_ru.qml.ts
