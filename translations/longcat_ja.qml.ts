5248384d27387d3ba335a2962ac59081537561f3
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-25 01:24:37 +0300

    Update translations (Japanese translation incomplete).

--- translations/longcat_ja.qml.ts
+++ translations/longcat_ja.qml.ts
@@ -2,9 +2,27 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
 <context>
+    <name>AdMobConsentDialog</name>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="30"/>
+        <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="62"/>
+        <source>Yes, show me relevant ads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="99"/>
+        <source>No, show me ads that are less relevant</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="8"/>
+        <location filename="../qml/main.qml" line="10"/>
         <source>Longcat</source>
         <translation>伸びいる</translation>
     </message>
--- translations/longcat_ja.ts
+++ translations/longcat_ja.ts
@@ -2,6 +2,21 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
 <context>
+    <name>AdMobConsentDialog</name>
+    <message>
+        <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes, show me relevant ads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No, show me ads that are less relevant</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
         <source>Longcat</source>
