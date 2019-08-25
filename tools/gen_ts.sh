51ec42ad56ab84daad49d8fe8e531e6c6eac33c2
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-25 19:47:13 +0300

    Always add locations to .ts files, regen translations.

--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -2,11 +2,11 @@
 
 PATH=$PATH:~/Qt/5.13.0/ios/bin
 
-lupdate ../longcat.pro -ts ../translations/longcat_ru.src.ts
-lupdate ../qml         -ts ../translations/longcat_ru.qml.ts
+lupdate -locations absolute ../longcat.pro -ts ../translations/longcat_ru.src.ts
+lupdate -locations absolute ../qml         -ts ../translations/longcat_ru.qml.ts
 
-lupdate ../longcat.pro -ts ../translations/longcat_ja.src.ts
-lupdate ../qml         -ts ../translations/longcat_ja.qml.ts
+lupdate -locations absolute ../longcat.pro -ts ../translations/longcat_ja.src.ts
+lupdate -locations absolute ../qml         -ts ../translations/longcat_ja.qml.ts
 
 lconvert ../translations/longcat_ru.src.ts ../translations/longcat_ru.qml.ts -o ../translations/longcat_ru.ts
 lconvert ../translations/longcat_ja.src.ts ../translations/longcat_ja.qml.ts -o ../translations/longcat_ja.ts
