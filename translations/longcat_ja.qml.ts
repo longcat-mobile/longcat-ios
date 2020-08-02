3f2caeb0b7e8d2d783625af3122a1e339367c026
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-08-02 23:35:09 +0300

    Update translations.

--- translations/longcat_ja.qml.ts
+++ translations/longcat_ja.qml.ts
@@ -4,17 +4,17 @@
 <context>
     <name>AdMobConsentDialog</name>
     <message>
-        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="30"/>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="33"/>
         <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt;</source>
         <translation>このアプリは広告を表示することで無料にしています。広告ネットワークは、&lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;データを収集し、デバイス上で一意の識別子を使用&lt;/a&gt;して広告を表示します。&lt;b&gt;データを使用して広告を調整することを許可しますか？&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="62"/>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="66"/>
         <source>Yes, show me relevant ads</source>
         <translation>はい、関連する広告を表示します</translation>
     </message>
     <message>
-        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="99"/>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="104"/>
         <source>No, show me ads that are less relevant</source>
         <translation>いいえ、関連性の低い広告を表示します</translation>
     </message>
